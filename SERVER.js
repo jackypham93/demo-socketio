211ecf7be9b7e33b153e111664a2e9758368616c
jackypham93 <phamphunghao93@gmail.com>
2018-11-03 12:07:48 +0700

    first commit

---
+++ SERVER.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var app = express();
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
+app.set("views","./views");
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(process.env.PORT || 3000);
+
+var mangUsers=[];
+
+io.on("connection", function(socket){
+  console.log("Co nguoi ket noi " + socket.id);
+  socket.on("client-send-Username",function(data){
+    if(mangUsers.indexOf(data) >= 0){
+      console.log(mangUsers.indexOf(data));
+      socket.emit("server-send-dki-thatbai");
+    }
+    else {
+      mangUsers.push(data);
+      socket.Username = data;
+      socket.emit("server-send-dki-thanhcong", data);
+      io.sockets.emit("server-send-danhsach-Users", mangUsers);
+      socket.on("logout", function(){
+        mangUsers.splice(mangUsers.indexOf(socket.Username), 1);
+        socket.broadcast.emit("server-send-danhsach-Users",mangUsers);
+      });
+
+      socket.on("user-send-message", function(data){
+      	
+        io.sockets.emit("server-send-message", {un: socket.Username, nd: data});
+      });
+
+
+
+      socket.on("toi-dang-go-chu", function(){
+      var s = socket.Username + " dang go chu";
+      io.sockets.emit("ai-do-dang-go-chu", s);
+      });
+      socket.on("toi-stop-go-chu", function(){
+        var s = socket.Username + " stop go chu";
+        io.sockets.emit("ai-do-da-stop-go-chu");
+      });
+    }
+  });
+});
+
+app.get("/", function(req, res){
+  res.render("trangchu");
+});
